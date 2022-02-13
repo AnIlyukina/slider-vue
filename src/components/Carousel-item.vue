d85e718ba637009a0f240aad0d4b248072a49535
AnnaIluykina <anyailyukina@yandex.ru>
2022-02-13 22:46:27 +0800

    Структура слайдера, создать компоненты Carousel и CarouselItem

---
+++ src/components/Carousel-item.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="carousel__item"> 
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Carousel-item',
+  props:{
+    item_data:{
+      type: Object,
+      default: () =>{}
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="carousel">
+    <CarouselItem/>
+  </section>
+</template>
+
+<script>
+import CarouselItem from './Carousel-item.vue'
+
+
+export default {
+  name:'Carousel',
+  components: { 
+    CarouselItem 
+  },
+  props:{
+    carousel_data:{
+      type: Array,
+      default: ()=>[]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
